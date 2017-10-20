ec1cdda740aba753ef9db67fe9a5253079b76491
Usuario <Usuario@10.0.1.56>
2017-10-20 12:07:03 -0300

    Signed-off-by: Usuario <Usuario@10.0.1.56>

--- src/graficos_v1/MostrarImagen.java
+++ src/graficos_v1/MostrarImagen.java
@@ -1,12 +1,42 @@
 package graficos_v1;
 
-public class MostrarImagen
+import java.awt.BorderLayout;
+import java.awt.Desktop;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.File;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MostrarImagen extends JFrame
 {
-
-	public static void main(String[] args)
+	private JPanel contentPane =new JPanel();
+	private void setear()
 	{
-		// TODO Auto-generated method stub
-
+		   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        setBounds(100, 100, 450, 300);
+	        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	        contentPane.setLayout(new BorderLayout(0, 0));
+	        setContentPane(contentPane);
+	        setBounds(0,0,800,600);
 	}
+	@Override
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+        Toolkit t = Toolkit.getDefaultToolkit();
 
+        Image imagen = t.getImage ("Desert.jpg");
+//        Desktop.getDesktop().open(imagen);
+        g.drawImage (imagen, 0, 0, this);
+	}
+	private void mostrar()
+	{
+		this.setear();
+		setVisible(true);
+	}
 }
